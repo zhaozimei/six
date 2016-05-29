9906ad45697e21228878cac484bfb30529a8ee25
zhaozimei <1140173180@qq.com>
2016-05-29 21:46:00 +0800

    Update practice.js

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var arr = [];
+	for(var i=0; i<collection_a.length; i++){
+		for(var j=0; j<collection_b.length; j++){
+			if(collection_a[i] == collection_b[j]){
+				arr.push(collection_a[i])
+			}
+		}
+	}
+	
+	return arr
 }
 
 module.exports = collect_same_elements;
